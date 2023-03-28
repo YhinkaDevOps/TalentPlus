19229c4652f4f5638ed6168bfa2296146b818c46
YhinkaDevOps <olayinka.bakare@scelloo.com>
2023-03-28 22:05:20 +0100

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,23 @@
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Techtime</title>
+        <meta
+          name="description"
+          content="Build your future with our quality education. the best and largest all-in-one online tutoring platform in the world"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </>
+  );
+}
